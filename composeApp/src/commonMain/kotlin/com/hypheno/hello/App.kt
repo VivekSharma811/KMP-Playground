090a128744f2ef5e18cd8fc4a54ef149545f100e
Vivek Sharma <vivek.sharma@mpokket.com>
2024-10-18 15:43:49 +0530

    Refactored

--- composeApp/src/commonMain/kotlin/com/hypheno/hello/App.kt
+++ composeApp/src/commonMain/kotlin/com/hypheno/hello/App.kt
@@ -1,101 +1,15 @@
 package com.hypheno.hello
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.hypheno.hello.network.InsultCensorClient
-import com.hypheno.hello.util.NetworkError
-import com.hypheno.hello.util.onError
-import com.hypheno.hello.util.onSuccess
-import kotlinx.coroutines.launch
+import com.hypheno.hello.ui.CensoredText
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App(client: InsultCensorClient) {
     MaterialTheme {
-        var censoredText by remember {
-            mutableStateOf<String?>(null)
-        }
-        var uncensoredText by remember {
-            mutableStateOf("")
-        }
-        var isLoading by remember {
-            mutableStateOf(false)
-        }
-        var errorMessage by remember {
-            mutableStateOf<NetworkError?>(null)
-        }
-        val scope = rememberCoroutineScope()
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
-        ) {
-            TextField(
-                value = uncensoredText,
-                onValueChange = { uncensoredText = it },
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                placeholder = {
-                    Text("Uncensored text")
-                }
-            )
-            Button(onClick = {
-                scope.launch {
-                    isLoading = true
-                    errorMessage = null
-
-                    client.censorWords(uncensoredText)
-                        .onSuccess {
-                            censoredText = it
-                        }
-                        .onError {
-                            errorMessage = it
-                        }
-                    isLoading = false
-                }
-            }) {
-                if(isLoading) {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .size(15.dp),
-                        strokeWidth = 1.dp,
-                        color = Color.White
-                    )
-                } else {
-                    Text("Censor!")
-                }
-            }
-            censoredText?.let {
-                Text(it)
-            }
-            errorMessage?.let {
-                Text(
-                    text = it.name,
-                    color = Color.Red
-                )
-            }
-        }
+        CensoredText(client)
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/hypheno/hello/ui/CensoredText.kt
@@ -0,0 +1,102 @@
+package com.hypheno.hello.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.hypheno.hello.network.CensoredText
+import com.hypheno.hello.network.InsultCensorClient
+import com.hypheno.hello.util.NetworkError
+import com.hypheno.hello.util.onError
+import com.hypheno.hello.util.onSuccess
+import kotlinx.coroutines.launch
+
+@Composable
+fun CensoredText(
+    client: InsultCensorClient,
+    modifier: Modifier = Modifier
+) {
+    var censoredText by remember {
+        mutableStateOf<String?>(null)
+    }
+    var uncensoredText by remember {
+        mutableStateOf("")
+    }
+    var isLoading by remember {
+        mutableStateOf(false)
+    }
+    var errorMessage by remember {
+        mutableStateOf<NetworkError?>(null)
+    }
+    val scope = rememberCoroutineScope()
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            16.dp,
+            Alignment.CenterVertically
+        )
+    ) {
+        TextField(
+            value = uncensoredText,
+            onValueChange = { uncensoredText = it },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            placeholder = {
+                Text("Uncensored text")
+            }
+        )
+        Button(onClick = {
+            scope.launch {
+                isLoading = true
+                errorMessage = null
+
+                client.censorWords(uncensoredText)
+                    .onSuccess {
+                        censoredText = it
+                    }
+                    .onError {
+                        errorMessage = it
+                    }
+                isLoading = false
+            }
+        }) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(15.dp),
+                    strokeWidth = 1.dp,
+                    color = Color.White
+                )
+            } else {
+                Text("Censor!")
+            }
+        }
+        censoredText?.let {
+            Text(it)
+        }
+        errorMessage?.let {
+            Text(
+                text = it.name,
+                color = Color.Red
+            )
+        }
+    }
+}
